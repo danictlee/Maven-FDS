03b6983bf508c3355c051150a31fda46fb79977c
Daniel  Lee <danictlee@gmail.com>
2024-03-27 01:13:54 +0000

    Feito

---
+++ maven/target/classes/br/pucrs/danictlee/Conta.java
@@ -0,0 +1,15 @@
+public class Conta {
+    public class Conta {
+        private double saldo;
+        public double getSaldo() {
+        return saldo;
+        }
+        public void depositar(double valor) {
+        saldo = saldo + valor;
+        }
+        public void sacar(double valor) {
+        if(valor <= saldo)
+        saldo = saldo - valor;
+        }
+    }
+}
